1631f51344bbeac40b1e781a1a1f25bca6333639
DESKTOP-TSEO09P\Shamim <shamim.ict0754@gmail.com>
2019-05-27 22:34:57 +0600

    remove theme

--- wp-content/themes/iqra-education/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* The sidebar containing the main widget area
-*/
-?>
-
-<aside id="secondary" class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Blog Sidebar', 'iqra-education' ); ?>">
-	<?php dynamic_sidebar( 'sidebox-1' ); ?>
-</aside>
